29f3360579327792f91927a48fc65b03a2c9dd96
Rane Fields <rane.fields@gmail.com>
2018-01-22 23:11:52 -0800

    Compose and display blog posts

---
+++ PortfolioSite/Controllers/CommentController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PortfolioSite.Data;
+
+namespace PortfolioSite.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CommentController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [Route("/posts/{postId}/comments")]
+        public IActionResult GetCommentsForPost(int postId)
+        {
+            return View();
+        }
+
+        [HttpPost, Route("/posts/{postId}/comments/add")]
+        public IActionResult AddCommentToPost(int postId)
+        {
+            return View();
+        }
+    }
+}
---
+++ PortfolioSite/Controllers/PostController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PortfolioSite.Data;
+using PortfolioSite.Models;
+
+namespace PortfolioSite.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PostController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [Route("/posts")]
+        public IActionResult Index()
+        {
+            List<BlogPost> model =_db.BlogPosts.ToList();
+            return View(model);
+        }
+
+        [Route("/posts/{postId}")]
+        public IActionResult GetPost(int postId)
+        {
+            BlogPost model = _db.BlogPosts.FirstOrDefault(m => m.Id == postId);
+            return View(model);
+        }
+
+        [Route("/posts/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost, Route("/posts/add")]
+        public IActionResult Add(BlogPost newPost)
+        {
+            _db.BlogPosts.Add(newPost);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
