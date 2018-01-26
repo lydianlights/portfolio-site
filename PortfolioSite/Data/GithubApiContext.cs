311a136e5a6cb7e83bec60dc06b3dfbcc3ec145c
Rane Fields <rane.fields@gmail.com>
2018-01-26 10:42:13 -0800

    Add context to talk to github api

---
+++ PortfolioSite/Data/GithubApiContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using PortfolioSite.Models;
+
+
+namespace PortfolioSite.Data
+{
+    public static class GithubApiContext
+    {
+        private const string apiUrl = "https://api.github.com";
+        private const string acceptHeader = "application/vnd.github.v3+json";
+        private const string myUserName = "ranefields";
+
+        public static async Task<List<Repo>> GetMyTop3StarredAsync()
+        {
+            var client = new RestClient(apiUrl);
+            var request = new RestRequest("/search/repositories", Method.GET);
+            request.AddHeader("Accept", acceptHeader);
+            request.AddParameter("q", $"user:{myUserName} fork:true");
+            request.AddParameter("sort", "stars");
+            request.AddParameter("order", "desc");
+
+            var response = await GetResponseAsync(client, request) as RestResponse;
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            IQueryable<Repo> repoResults = JsonConvert.DeserializeObject<IQueryable<Repo>>(jsonResponse["items"].ToString());
+
+            List<Repo> top3Repos = repoResults.Take(3).ToList();
+            return top3Repos;
+        }
+
+        private static Task<IRestResponse> GetResponseAsync(RestClient client, RestRequest request)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            client.ExecuteAsync(request, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
---
+++ PortfolioSite/Models/Repo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortfolioSite.Models
+{
+    public class Repo
+    {
+        public string Name { get; set; }
+        public string Html_Url { get; set; }
+        public int Stargazers_Count { get; set; }
+        public DateTime Updated_At { get; set; }
+        public string Language { get; set; }
+    }
+}
