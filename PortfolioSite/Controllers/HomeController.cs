1341af43119e292be3c946c64bfcf7bc5bd28a44
Rane Fields <rane.fields@gmail.com>
2018-01-26 15:53:16 -0800

    Add api call to get project info

--- PortfolioSite/Controllers/HomeController.cs
+++ PortfolioSite/Controllers/HomeController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PortfolioSite.Data;
+using PortfolioSite.Models;
 
 namespace PortfolioSite.Controllers
 {
     public class HomeController : Controller
     {
+        // TODO: Find a way to load rest of page while waiting for async api call
         [Route("/")]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<Repo> model = await GithubApiContext.GetMyTop3StarredAsync();
+            return View(model);
         }
     }
 }
